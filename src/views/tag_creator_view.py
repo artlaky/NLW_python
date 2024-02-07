2de19e1b0c4ac248e281afe60b5be3e0446a99c3
Lucas Queiroz <lucas.queiroz@ig.ufpa.br>
2024-02-07 00:40:19 -0300

    feat: Adding crontrollers and drivers for the project

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controler import TagCreatorControler
 
 class TagCreatorView:
     '''
@@ -7,11 +8,13 @@ class TagCreatorView:
     '''
 
     def validade_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controler = TagCreatorControler()
+        
         body= http_request.body
         product_code = body["product_code"]
 
         # logica de regra de negocio
-        print("Estou na minha view")
-        print(product_code)
+        formatted_response = tag_creator_controler.create(product_code)
+
         # retorno HTPP
-        return HttpResponse(status_code=200, body={"Hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
